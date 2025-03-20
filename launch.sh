359e18cefb0b50767a1650fba5ecc7a763b1e066
cryptomoondog <59893909+Arieitovitch@users.noreply.github.com>
2025-03-20 12:49:14 -0400

    add

--- launch.sh
+++ launch.sh
@@ -1,22 +1,21 @@
+
 #!/bin/bash
-#SBATCH -p all # partition (queue) 
-#SBATCH -c 4 # number of cores 
-#SBATCH --mem=100G 
-#SBATCH --propagate=NONE 
-# IMPORTANT for long jobs 
-#SBATCH -t 1-00:00 # time (D-HH:MM) 
-#SBATCH --mail-user=arie.itovitch@mail.mcgill.ca
+#SBATCH -w gpu-grad-01
+#SBATCH --ntasks 8
+#SBATCH --mem 32GB
+#SBATCH -t 0-2:00
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+#SBATCH --mail-user=<your_name>@mail.mcgill.ca
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --output=output.log 
 #SBATCH --error=error.log 
 #SBATCH --qos=comp579-0gpu-4cpu-72h 
 #SBATCH --account=winter2025-comp579
 
-
 module load cuda/cuda-12.6
-module load python/3.10
-
 pip3 cache purge
 pip3 install -r requirements.txt
+python3 -c "import sys; print(sys.path)"
 
 python3 a3.py
