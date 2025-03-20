ba1493e81b901f362fb0555749cc898bf4d56d17
cryptomoondog <59893909+Arieitovitch@users.noreply.github.com>
2025-03-20 12:12:50 -0400

    fuck vim

---
+++ launch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -p all # partition (queue) 
+#SBATCH -c 4 # number of cores 
+#SBATCH --mem=100G 
+#SBATCH --propagate=NONE 
+# IMPORTANT for long jobs 
+#SBATCH -t 1-00:00 # time (D-HH:MM) 
+#SBATCH --mail-user=arie.itovitch@mail.mcgill.ca
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=output.log 
+#SBATCH --error=error.log 
+#SBATCH --qos=comp579-0gpu-4cpu-72h 
+#SBATCH --account=winter2025-comp579
+
+
+module load cuda/cuda-12.6
+module load python/3.10
+
+pipi3 cache purge
+pip3 install -r requirements.txt
+
+python3 a3.py
